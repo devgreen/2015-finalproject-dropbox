d93ccddb568fdbad13dcc3a423558d682722541e
devgreen <dgreen2197@gmail.com>
2015-06-05 12:43:25 -0400

    middle of automatic download

--- src/clientServerConnection/CommandThread.java
+++ src/clientServerConnection/CommandThread.java
@@ -1,8 +1,11 @@
 package clientServerConnection;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandThread extends Thread {
 
@@ -24,6 +27,16 @@ public class CommandThread extends Thread {
 	public void run() {
 
 		writer.println("LIST");
+		List<File> serverFiles = server.getFileCache().getFiles();
+		while (true){
+			if ( !serverFiles.equals(server.getFileCache().getFiles())){
+				List <File> missing = new ArrayList(server.getFileCache().getFiles());
+				missing.removeAll(serverFiles);
+				for (int i = 0; i<missing.size(); i++){
+					writer.println("DOWNLOAD");
+				}
+			}
+		}
 		
 	}
 
